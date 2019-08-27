a1ea2d88364bc2f89315ca9bcc1a939b203cc7f9
Christian Emmer <emmercm@gmail.com>
2019-08-27 14:16:11 -0700

    Powerline fonts install.

--- .powerline.bash
+++ .powerline.bash
@@ -1,5 +1,14 @@
 # pip3 install --user powerline-status powerline-gitstatus
 
+# Install
+if [[ -d ~/Library/Fonts && "$(find ~/Library/Fonts -maxdepth 1 -follow -type f -name "*Powerline*")" == "" ]]; then
+    git clone https://github.com/powerline/fonts.git --depth=1
+    cd fonts
+    ./install.sh
+    cd ..
+    rm -rf fonts
+fi
+
 if [[ -x "$(command -v powerline-daemon)" ]]; then
     powerline-daemon --quiet
     POWERLINE_BASH_CONTINUATION=1
