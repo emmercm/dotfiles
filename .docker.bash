eacfa83cbfa89004cba7237006d6c66dbeb778d9
Christian Emmer <christianemmer@stockx.com>
2019-12-12 11:44:54 -0500

    Docker (#8)
    * Separate Docker alias file.
    
    * Docker dhash

--- .docker.bash
+++ .docker.bash
@@ -1,13 +1,14 @@
-# Execute `sh` interactively in the Docker container
-# @param {string} $1 Container name
-dsh() {
-    docker exec --interactive --tty "$1" -- sh
-}
-
 # Execute `bash` interactively in the Docker container
 # @param {string} $1 Container name
 dbash() {
     docker exec --interactive --tty "$1" -- bash
+}
+
+# Get the digest hash of a Docker image
+# @param {string} $1 name[:tag|@digest]
+dhash() {
+    docker pull "$1" &> /dev/null || true
+    docker inspect --format='{{index .RepoDigests 0}}' "$1" | awk -F "@" '{print $2}'
 }
 
 # Kill all running Docker containers
@@ -17,3 +18,9 @@ dkillall() {
 
 # Kill all running Docker containers and delete all container data
 alias dprune="dkillall && docker system prune --all --force && docker images purge"
+
+# Execute `sh` interactively in the Docker container
+# @param {string} $1 Container name
+dsh() {
+    docker exec --interactive --tty "$1" -- sh
+}
