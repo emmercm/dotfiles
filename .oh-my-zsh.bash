d63d23dd3a873047c20e2658453d78c82dd49498
Christian Emmer <emmercm@gmail.com>
2020-10-06 18:47:22 -0400

    oh-my-zsh tweaks.

--- .oh-my-zsh.bash
+++ .oh-my-zsh.bash
@@ -51,6 +51,30 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Load the theme
 antigen theme romkatv/powerlevel10k
+if [[ -d ~/Library/Fonts && "$(find ~/Library/Fonts -maxdepth 1 -follow -type f -name "*MesloLGS NF*")" == "" ]]; then
+    git clone https://github.com/romkatv/powerlevel10k-media.git --depth=1
+    cd powerlevel10k-media
+    cp *.ttf ~/Library/Fonts
+    cd ..
+    rm -rf powerlevel10k-media
+	osascript <<- EOF
+		tell application "Terminal"
+			set ProfilesNames to name of every settings set
+			repeat with ProfileName in ProfilesNames
+				set font name of settings set ProfileName to "MesloLGS NF"
+			end repeat
+		end tell
+	EOF
+fi
 
 # Tell Antigen that you're done
 antigen apply
+
+# Fix some insecure directories?
+# https://github.com/zsh-users/zsh-completions/issues/433
+if [[ -x "$(command -v compaudit)" ]]; then
+    while read -r FILE; do
+        sudo chown -R "$(whoami)" "${FILE}"
+        sudo chmod -R 755 "${FILE}"
+    done <<< "$(compaudit 2> /dev/null)"
+fi
