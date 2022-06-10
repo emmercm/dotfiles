7fb8dd7119e92d2d4319757c017988a9744da61e
Christian Emmer <emmercm@gmail.com>
2022-06-10 13:06:03 -0700

    ShellCheck

--- .20_nodejs.bash
+++ .20_nodejs.bash
@@ -12,20 +12,21 @@ __nodejs_funcs() {
     # @param {string=} $1 Package name
     nengine() {
         if [[ $# -eq 0 ]]; then
-            DIRS=$(npm ls --parseable | tail -n +2)
+            DIRS=$(npm ls --parseable 2> /dev/null | tail -n +2)
             RANGES=$(echo "${DIRS}" | while read -r DIR; do jq -r '.engines.node' "${DIR}/package.json" 2> /dev/null; done | grep -v null | sort --unique)
         else
             DEPS=$(npm-remote-ls "$1" | tail -n +2 | sed 's/^[^a-z0-9]*//' | sed 's/[ ].*//' | sort --unique)
-            RANGES=$(echo "${DEPS}" | while read -r DEP; do npm view "${DEP}" engines.node; done)
+            RANGES=$(echo "${DEPS}" | while read -r DEP; do npm view "${DEP}" engines.node; done;)
         fi
 
         # Simplify ^ and >= ranges
-        RANGES=$(echo "${RANGES}" | sed 's/>=[ ]*//g' | sed 's/\^//g' | sort --unique)
+        RANGES=$(echo "${RANGES}" | sed 's/>=\?\s\?//g' | sed 's/\^//g' | sort --unique)
 
         # Parse || ranges
         RANGES=$(echo "${RANGES}" | while read -r RANGE; do echo "${RANGE}" | sed -e $'s/\|\|/\\\n/g' | sed 's/^ *//;s/ *$//' | sort --version-sort | head -1; done | sort --unique)
 
         # Simplify wildcards
+        # shellcheck disable=SC2063
         RANGES=$(echo "${RANGES}" | sed 's/\(\.[\*x]\)*$//' | grep -v "*" | sort --unique)
 
         echo "${RANGES}" | sort --version-sort | tail -1
