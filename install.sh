432ac9e8eedb79f8f06508543a086795e71410fa
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2025-08-14 15:34:19 -0700

    8/14/2025

--- install.sh
+++ install.sh
@@ -19,31 +19,35 @@ function backup() {
 # Given a path and expression, safely link all files to the home directory
 function link() {
     # Create symlinks
-    while read -r FILE; do
-        LINK="${HOME}/$(basename "${FILE}")"
+    while read -r file; do
+        local link="${HOME}/$(basename "${file}")"
 
         # Ensure symlink is actually a dotfile
-        if [[ "$(basename "${LINK}")" != .* ]]; then
-            LINK="$(dirname "${LINK}")/.$(basename "${LINK}")"
+        if [[ "$(basename "${link}")" != .* ]]; then
+            link="$(dirname "${link}")/.$(basename "${link}")"
         fi
 
         # Assume symlinks are ok
-        if [[ -h "${LINK}" ]]; then
+        if [[ -h "${link}" ]]; then
+            echo "Ignoring: ${link}"
             continue
         fi
 
         # Back up the existing file
-        if [[ -e "${LINK}" ]]; then
-            mv "${LINK}" "$(backup "${LINK}")"
+        if [[ -e "${link}" ]]; then
+            local backup="$(backup "${link}")"
+            echo "Moving: ${link} -> ${backup}"
+            mv "${link}" "${backup}"
         fi
 
         # Symlink the file
-        ln -s "${FILE}" "${LINK}"
+        echo "Linking: ${link} -> ${file}"
+        ln -s "${file}" "${link}"
     done <<< "$(find "$1" -maxdepth 1 -name "$2" ! -name ".git" ! -name ".github" ! -name ".gitignore")"
 
     # Delete broken symlinks
-    while read -r FILE; do
-        rm -f "${FILE}"
+    while read -r file; do
+        rm -f "${file}"
     done <<< "$(find "${HOME}" -maxdepth 1 -type l ! -exec test -e {} \; -print)"
 }
 
