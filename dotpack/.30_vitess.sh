5b75810bf21d65195c87826a4638eb72f24bb4f8
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-05 11:32:06 -0800

    2/5/2026

--- dotpack/.30_vitess.sh
+++ dotpack/.30_vitess.sh
@@ -1,6 +1,19 @@
 ##### https://vitess.io/
 ##### https://planetscale.com/
 
+__vitess_completions() {
+    if command -v pscale &> /dev/null; then
+        pscale() {
+            unset -f "$0"
+            # https://planetscale.com/docs/cli/completion
+            source <(pscale completion "$(basename "${SHELL}")")
+            $0 "$@"
+        }
+    fi
+}
+__vitess_completions
+
+
 __vitess_lazy_install() {
     # https://planetscale.com/docs/cli/planetscale-environment-setup#macos-instructions
     if ! command -v pscale &> /dev/null && command -v brew &> /dev/null; then
